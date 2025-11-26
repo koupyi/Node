e68b6ee6e9b41443a7b9917171b6ec4124ec6554
koupyi <hector.oliveira5@etec.sp.gov.br>
2025-11-25 23:30:29 -0300

    realtime database

--- Atividade/realtime_database/app.js
+++ Atividade/realtime_database/app.js
@@ -59,7 +59,9 @@ app.use((req, res) => {
 });
 
 // -------------------- SERVIDOR --------------------
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
+
